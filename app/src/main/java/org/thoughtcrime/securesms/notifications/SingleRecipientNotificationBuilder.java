c537da6acd3047fa93f7e8ef7036463075d57ab0
charles <charles@oxen.io>
2022-10-03 16:31:29 +1100

    fix: Show message sender in push notifications for groups

--- app/src/main/java/org/thoughtcrime/securesms/notifications/SingleRecipientNotificationBuilder.java
+++ app/src/main/java/org/thoughtcrime/securesms/notifications/SingleRecipientNotificationBuilder.java
@@ -117,15 +117,15 @@ public class SingleRecipientNotificationBuilder extends AbstractNotificationBuil
     setNumber(messageCount);
   }
 
-  public void setPrimaryMessageBody(@NonNull  Recipient threadRecipients,
+  public void setPrimaryMessageBody(@NonNull  Recipient threadRecipient,
                                     @NonNull  Recipient individualRecipient,
                                     @NonNull  CharSequence message,
                                     @Nullable SlideDeck slideDeck)
   {
     SpannableStringBuilder stringBuilder = new SpannableStringBuilder();
 
-    if (privacy.isDisplayContact() && threadRecipients.isOpenGroupRecipient()) {
-      String displayName = getOpenGroupDisplayName(individualRecipient);
+    if (privacy.isDisplayContact() && threadRecipient.isGroupRecipient()) {
+      String displayName = getGroupDisplayName(individualRecipient, threadRecipient.isOpenGroupRecipient());
       stringBuilder.append(Util.getBoldedString(displayName + ": "));
     }
 
@@ -214,8 +214,8 @@ public class SingleRecipientNotificationBuilder extends AbstractNotificationBuil
   {
     SpannableStringBuilder stringBuilder = new SpannableStringBuilder();
 
-    if (privacy.isDisplayContact() && threadRecipient.isOpenGroupRecipient()) {
-      String displayName = getOpenGroupDisplayName(individualRecipient);
+    if (privacy.isDisplayContact() && threadRecipient.isGroupRecipient()) {
+      String displayName = getGroupDisplayName(individualRecipient, threadRecipient.isOpenGroupRecipient());
       stringBuilder.append(Util.getBoldedString(displayName + ": "));
     }
 
@@ -334,14 +334,15 @@ public class SingleRecipientNotificationBuilder extends AbstractNotificationBuil
   }
 
   /**
-   * @param recipient the * individual * recipient for which to get the open group display name.
+   * @param recipient          the * individual * recipient for which to get the display name.
+   * @param openGroupRecipient whether in an open group context
    */
-  private String getOpenGroupDisplayName(Recipient recipient) {
+  private String getGroupDisplayName(Recipient recipient, boolean openGroupRecipient) {
     SessionContactDatabase contactDB = DatabaseComponent.get(context).sessionContactDatabase();
     String sessionID = recipient.getAddress().serialize();
     Contact contact = contactDB.getContactWithSessionID(sessionID);
     if (contact == null) { return sessionID; }
-    String displayName = contact.displayName(Contact.ContactContext.OPEN_GROUP);
+    String displayName = contact.displayName(openGroupRecipient ? Contact.ContactContext.OPEN_GROUP : Contact.ContactContext.REGULAR);
     if (displayName == null) { return sessionID; }
     return displayName;
   }
